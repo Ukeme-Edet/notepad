776d8eee2a404b67e09b2b9f3fc3e5003b8e451b
Ukeme Edet <ukemeedet2207@gmail.com>
2023-03-24 08:04:39 +0100

    Styled the Content Select Component

--- src/app/components/content-select/content-select.component.ts
+++ src/app/components/content-select/content-select.component.ts
@@ -1,22 +1,35 @@
 import { Component } from '@angular/core';
+import {
+  faCircleCheck as faCircleCheckRegular,
+  faRectangleList as faRectangleListRegular,
+} from '@fortawesome/free-regular-svg-icons';
+import {
+  faCircleCheck as faCircleCheckSolid,
+  faRectangleList as faRectangleListSolid,
+} from '@fortawesome/free-solid-svg-icons';
 import { AppStateService } from 'src/app/services/app-state/app-state.service';
 
 @Component({
   selector: 'app-content-select',
   templateUrl: './content-select.component.html',
-  styleUrls: ['./content-select.component.css']
+  styleUrls: ['./content-select.component.css'],
 })
 export class ContentSelectComponent {
+  faCircleCheckRegular = faCircleCheckRegular;
+  faCircleCheckSolid = faCircleCheckSolid;
+  faRectangleListRegular = faRectangleListRegular;
+  faRectangleListSolid = faRectangleListSolid;
+  appState!: AppStateService;
 
   constructor(private appStateService: AppStateService) {
+    this.appState = appStateService;
   }
 
   activateNotesMode() {
-    this.appStateService.toogleViewMode("notes");
+    this.appStateService.toogleViewMode('notes');
   }
 
   activateTasksMode() {
-    this.appStateService.toogleViewMode("tasks");
+    this.appStateService.toogleViewMode('tasks');
   }
-
 }
