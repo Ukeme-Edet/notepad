0cd284f5b8617bbe87da899b92e0dd4e3cc76ab5
Ukeme Edet <ukemeedet2207@gmail.com>
2023-04-12 16:05:58 +0100

    Task Edti Fully Implemented

--- src/app/components/tasks-list/tasks-list.component.ts
+++ src/app/components/tasks-list/tasks-list.component.ts
@@ -1,5 +1,7 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { map } from 'rxjs';
+import { Task } from 'src/app/interfaces/task.model';
+import { AppStateService } from 'src/app/services/app-state/app-state.service';
 import { ContentDatabaseService } from 'src/app/services/content-database/content-database.service';
 
 @Component({
@@ -11,5 +13,9 @@ import { ContentDatabaseService } from 'src/app/services/content-database/conten
 export class TasksListComponent {
   tasks$ = this.contentDatabaseService.getTasks().pipe(map((data) => data));
 
-  constructor(private contentDatabaseService: ContentDatabaseService) {}
+  constructor(private contentDatabaseService: ContentDatabaseService, private appStateService: AppStateService) {}
+
+  onTaskClick(task: Task) {
+    this.appStateService.setActiveTaskId(task.id);
+  }
 }
